1274cafed2de93c05a4b01e91bef7ba5c44bdb6a
Guillermo Weyler <guillermo.weyler.cabrera@gmail.com>
2025-11-26 16:21:25 +0000

    Setup Pacakges and enpoints for now

--- Program.cs
+++ Program.cs
@@ -1,6 +1,34 @@
+using Serilog;
+using Mapster;
+using BG_Tec_Assesment_Minimal_Api.Data;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
+builder.Services.AddDbContext<TravellerAPIDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddMapster();
+
+builder.Services.AddOpenApi();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/checkin", () => "Check -in endpoint placeholder");
+
+app.MapGet("/traveller/{id}", (int id) => "Traveller endpoint placeholder");
+
+app.MapGet("/traveller/search", () => " search endpoint ");
+
 
 app.Run();
